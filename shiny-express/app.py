ab958780c9bc7c38f0a3af91f291750a5cecb161
vivid-storage <165187161+vivid-storage@users.noreply.github.com>
2024-04-11 19:45:49 -0500

    Update app.py

--- shiny-express/app.py
+++ shiny-express/app.py
@@ -5,11 +5,11 @@ from shinywidgets import render_plotly
 ui.page_opts(title="Penguins dashboard", fillable=True)
 
 with ui.sidebar():
-    ui.input_selectize(
+    input.input_selectize(
         "var", "Select variable",
         ["bill_length_mm", "bill_depth_mm", "flipper_length_mm", "body_mass_g", "year"]
     )
-    ui.input_numeric("bins", "Number of bins", 30)
+    input.input_numeric("bins", "Number of bins", 30)
 
 @reactive.calc
 def df():
@@ -20,6 +20,6 @@ with ui.card(full_screen=True):
     @render_plotly
     def hist():
         import plotly.express as px
-        p = px.histogram(df(), nbins=input.bins())
+        p = px.histogram(df(), x=input.var(), nbins=input.bins())
         p.layout.update(showlegend=False)
         return p
